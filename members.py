e630ad7164be8b6676cab4b8e80e9d8070392e77
Shreshth <skuma841@mtroyal.ca>
2022-09-19 11:48:31 -0600

    Shreshth init push

--- members.py
+++ members.py
@@ -1,7 +1,9 @@
 messageNate = "Hello World from Nate! UWU"
 messageKyle = "Hello World from Kyle! OwO"
 messageBrandon = "Heyyyy World from Brandon! lol"
+messageShreshth = "Whats up world, its your leader, Shreshth!. NOW KNEEL!"
 
 print(messageNate)
 print(messageKyle)
 print(messageBrandon)
+print(messageShreshth)
