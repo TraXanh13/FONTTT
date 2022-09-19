06becee57b6bcf0f3d59c56921f1a09f489272c2
Brandon Chan <brandon.nic99@gmail.com>
2022-09-19 11:47:13 -0600

    heyyyyy

--- members.py
+++ members.py
@@ -1,5 +1,7 @@
 messageNate = "Hello World from Nate! UWU"
 messageKyle = "Hello World from Kyle! OwO"
+messageBrandon = "Heyyyy World from Brandon! lol"
 
 print(messageNate)
 print(messageKyle)
+print(messageBrandon)
