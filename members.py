ceb9eba65b33b46d209907cb7ac52f2877983e23
Chris Stepien <31741153+Cstep212@users.noreply.github.com>
2022-09-19 11:50:39 -0600

    Hello!

--- members.py
+++ members.py
@@ -2,8 +2,10 @@ messageNate = "Hello World from Nate! UWU"
 messageKyle = "Hello World from Kyle! OwO"
 messageBrandon = "Heyyyy World from Brandon! lol"
 messageShreshth = "Whats up world, its your leader, Shreshth!. NOW KNEEL!"
+messageChris = "Hello World from Chris! O_O"
 
 print(messageNate)
 print(messageKyle)
 print(messageBrandon)
 print(messageShreshth)
+print(messageChris)
